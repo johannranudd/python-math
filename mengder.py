4c514aad297ef9995e7f854398522c78a8f41c40
johann ranudd <johnrand380@gmail.com>
2024-12-04 15:26:01 +0100

    adds amount calculations

---
+++ mengder.py
@@ -0,0 +1,7 @@
+A = {2,5,8}
+B = {4,5,6,7}
+
+print("Snittet av A og B er", A.intersection(B))
+print("Unionen av A og B er", A.union(B))
+print("B frakturert A er", B.difference(A))
+print("A frakturert B er", A.difference(B))
